075c24f5723ba00536633a86ba1c57ae4f4dc481
geekyNischal <mailfornischal@gmail.com>
2022-03-26 19:44:19 +0530

    New build.

--- src/components/Feed/index.tsx
+++ src/components/Feed/index.tsx
@@ -13,11 +13,13 @@ export const Feed: React.FC<FeedProps> = ({ allLines, shareOnTwitter }) => {
     <div id="feed" className="feed-tab">
       <div className="feed-heading">
         <Clock color="#d1495b" size={32} weight="fill" />
-        <h3>Recent <span className="feed-emphasis">Posts</span></h3>
+        <h3>
+          Recent <span className="feed-emphasis">Posts</span>
+        </h3>
       </div>
-      {allLines.length > 0 ? (
+      {allLines.length > 0 && (
         <Cards allLines={allLines} shareOnTwitter={shareOnTwitter} />
-      ) : <p>We're working on this...give us a moment.</p>}
+      )}
     </div>
   );
 };
